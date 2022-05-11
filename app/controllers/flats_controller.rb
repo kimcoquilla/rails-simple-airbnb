8afb184f5c4facb765cabef569f902b095c020b5
Kim Coquilla <kimcoquilla@gmail.com>
2022-05-11 16:40:40 -0400

    Simple Airbnb just started

--- app/controllers/flats_controller.rb
+++ app/controllers/flats_controller.rb
@@ -1,2 +1,35 @@
 class FlatsController < ApplicationController
+  def index
+    @flats = Flat.all
+  end
+
+  def new
+    @flat = Flat.new
+  end
+
+  def create
+    @flat = Flat.new(params.require(:flat).permit(:name, :address, :description, :price_per_night, :number_of_guests))
+    @flat.save
+    redirect_to flat_path(@flat)
+  end
+
+  def show
+    @flat = Flat.find(params[:id])
+  end
+
+  def edit
+    @flat = Flat.find(params[:id])
+  end
+
+  def update
+    @flat = Flat.find(params[:id])
+    @flat.update(params.require(:flat).permit(:name, :address, :description, :price_per_night, :number_of_guestst))
+    redirect_to flat_path(@flat)
+  end
+
+  def destroy
+    article = Flat.find(params[:id])
+    article.destroy
+    redirect_to flats_path
+  end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+5.times do
+  Flat.create!(
+    name: Faker::Company.name,
+    address: Faker::Address.street_address,
+    description: Faker::ChuckNorris.fact,
+    price_per_night: rand(75..99),
+    number_of_guests: rand(3..10)
+  )
+end
+
+puts 'Done'
